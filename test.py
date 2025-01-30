378350b274c1f19c47c40a9881f8bc88ddec5d43
ChoyonUddin <choyonuddin17@gmail.com>
2025-01-30 05:44:20 -0500

    Ok I messed up and I probably wont fix it

--- test.py
+++ test.py
@@ -1,11 +1,26 @@
-nums = [1,2,3,4,5]
-res = []
-count = 0
-while sum(nums) > sum(res):  
-    res.append(nums[count])
-    count += 1
-    nums = nums[1:]
-    print(nums.pop(1))
-print(nums)
+# nums = [1,2,3,4,5]
+# res = []
+# count = 0
+# while sum(nums) > sum(res):  
+#     res.append(nums[count])
+#     count += 1
+#     nums = nums[1:]
+#     print(nums.pop(1))
+# print(nums)
+# res,curr = [],0
+#         if len(nums) <=1:
+#             return sum(nums)
+#         for i in range(len(nums)-1):
+#             res.append(nums[i])
+#             if len(res) == k:
+#                 if curr < sum(res)/k:
+#                     curr = sum(res)/k
+#                 res.pop(0)
+            
+#         return curr
 
+s = 'The Sky is Blue'
+s= s.split(" ")
+s = ' '.join(reversed(s))
+print(s)
 # Path: test.py
